e8d733620bd5c91e9ced9c8cb25bd798c7dfded0
Peter Heiner <p.heiner@gmail.com>
2015-02-24 12:52:21 +0000

    add templates to config manifest

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,15 @@
 class sssd::config (
 
 ) {
-
+    file {
+        '/etc/realmd.conf':
+            ensure => file,
+            content => template('sssd/realmd.conf.erb');
+        '/etc/sssd':
+            ensure => directory;
+        '/etc/sssd/sssd.conf':
+            ensure => file,
+            content => template('sssd/sssd.conf.erb'),
+            require => File['/etc/sssd'];
+    }
 }
